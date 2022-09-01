42c09a05b674fae7df33e8e38b5d62033a85a8b3
CapserFriendlyGhost <kgrochowski94@gmail.com>
2022-09-01 23:22:21 +0200

    01/09/22

--- pokemon-app/src/pages/Home.js
+++ pokemon-app/src/pages/Home.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import PokeCard from "../components/PokeCard";
 import FetchApi from "../components/FetchApi";
 import { Typography, CircularProgress, Box } from "@mui/material/";
 import styled from "styled-components";
+import ReactPaginate from "react-paginate";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { style } from "@mui/system";
 
 const S = {
   StyledCardsWrapper: styled.div`
@@ -11,9 +15,15 @@ const S = {
     justify-content: space-evenly;
     flex-wrap: wrap;
   `,
+
+  StyledReactPaginateWrapper: styled(Box)`
+    display: flex;
+    justify-content: center;
+  `,
 };
 
 const Home = ({ searchValue }) => {
+  const [pageNumber, setPageNumber] = useState(0);
   const { data, status } = useQuery("pokemonData", FetchApi, {
     refetchOnMount: false,
     refetchOnWindowFocus: false,
@@ -21,10 +31,6 @@ const Home = ({ searchValue }) => {
     staleTime: Infinity,
     cacheTime: Infinity,
   });
-
-  console.log(searchValue);
-  console.log(data);
-  console.log(useQuery("pokemonData", FetchApi));
 
   const filteredData = data?.filter((pokemon) => {
     if (searchValue === "") {
@@ -34,29 +40,87 @@ const Home = ({ searchValue }) => {
     }
   });
 
+  const pokemonPerPage = 15;
+  const pagesVisited = pageNumber * pokemonPerPage;
+
+  // const displayPokemons = filteredData
+  //   ?.slice(pagesVisited, pagesVisited + pokemonPerPage)
+  //   .map((pokemon) => {
+  //     return (
+  //       <PokeCard
+  //         key={pokemon.name}
+  //         name={pokemon.name}
+  //         img={pokemon.sprites.front_default}
+  //         height={pokemon.height}
+  //         weight={pokemon.weight}
+  //         exp={pokemon.base_experience}
+  //         ability={pokemon.abilities[0].ability.name}
+  //       />
+  //     );
+  //   });
+
+  const pageCount = Math.ceil(filteredData?.length / pokemonPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <Box margin={5}>
       {status === "loading" && <CircularProgress />}
       {status === "error" && <div> Error </div>}
       {status === "success" && (
         <S.StyledCardsWrapper>
-          {filteredData?.map((pokemon) => {
-            return (
-              <PokeCard
-                key={pokemon.name}
-                name={pokemon.name}
-                img={pokemon.sprites.front_default}
-                height={pokemon.height}
-                weight={pokemon.weight}
-                exp={pokemon.base_experience}
-                ability={pokemon.abilities[0].ability.name}
-              />
-            );
-          })}
+          {filteredData
+            ?.slice(pagesVisited, pagesVisited + pokemonPerPage)
+            .map((pokemon) => {
+              return (
+                <PokeCard
+                  key={pokemon.name}
+                  name={pokemon.name}
+                  img={pokemon.sprites.front_default}
+                  height={pokemon.height}
+                  weight={pokemon.weight}
+                  exp={pokemon.base_experience}
+                  ability={pokemon.abilities[0].ability.name}
+                />
+              );
+            })}
         </S.StyledCardsWrapper>
       )}
+      <S.StyledReactPaginateWrapper>
+        <ReactPaginate
+          previousLabel={<ArrowBackIcon />}
+          nextLabel={<ArrowForwardIcon />}
+          pageCount={pageCount}
+          onPageChange={changePage}
+        />
+      </S.StyledReactPaginateWrapper>
     </Box>
   );
 };
 
 export default Home;
+
+// return (
+//   <Box margin={5}>
+//     {status === "loading" && <CircularProgress />}
+//     {status === "error" && <div> Error </div>}
+//     {status === "success" && (
+//       <S.StyledCardsWrapper>
+//         {filteredData?.map((pokemon) => {
+//           return (
+//             <PokeCard
+//               key={pokemon.name}
+//               name={pokemon.name}
+//               img={pokemon.sprites.front_default}
+//               height={pokemon.height}
+//               weight={pokemon.weight}
+//               exp={pokemon.base_experience}
+//               ability={pokemon.abilities[0].ability.name}
+//             />
+//           );
+//         })}
+//       </S.StyledCardsWrapper>
+//     )}
+//   </Box>
+// );
