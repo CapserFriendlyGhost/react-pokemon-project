fe95c3f9efb43a7c4c5c3cd89d624e288b76bd24
CapserFriendlyGhost <kgrochowski94@gmail.com>
2022-09-23 16:13:42 +0200

    23/09/22

--- pokemon-app/src/components/FormTextField.js
+++ pokemon-app/src/components/FormTextField.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { ErrorMessage, useField } from "formik";
 import { TextField, Typography } from "@mui/material/";
+import styled from "styled-components";
+
+const S = {
+  StyledTextField: styled(TextField)`
+    background-color: #ffffef;
+    -webkit-box-shadow: 0 1px 10px -5px black;
+    -moz-box-shadow: 0 1px 10px -5px black;
+    box-shadow: 0 1px 10px -5px black;
+  `,
+};
 
 const FormTextField = ({ label, type, ...props }) => {
   const [field, meta] = useField(props);
   return (
     <>
-      <TextField
+      <S.StyledTextField
         label={label}
         name={field.name}
         variant="outlined"
