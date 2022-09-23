abd3555d3c02cc3f038865ed2711b68a08a05c08
CapserFriendlyGhost <kgrochowski94@gmail.com>
2022-09-23 14:45:45 +0200

    23/09/22

--- pokemon-app/src/components/Pagination.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-const usePagination = (data, itemsPerPage) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const pageCount = Math.ceil(data.length / itemsPerPage);
-  const end = begin + itemsPerPage;
-};
